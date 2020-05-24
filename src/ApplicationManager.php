431186dbd96dfaf61588c179087ea09b0ea37366
tkotosz <tkotosz@inviqa.com>
2020-05-24 14:00:52 +0200

    Support custom extension sources

--- src/ApplicationManager.php
+++ src/ApplicationManager.php
@@ -7,6 +7,8 @@ use Tkotosz\CliAppWrapperApi\ApplicationConfig;
 use Tkotosz\CliAppWrapperApi\ApplicationManager as ApplicationManagerInterface;
 use Tkotosz\CliAppWrapperApi\Extension;
 use Tkotosz\CliAppWrapperApi\Extensions;
+use Tkotosz\CliAppWrapperApi\ExtensionSource;
+use Tkotosz\CliAppWrapperApi\ExtensionSources;
 use Tkotosz\ComposerWrapper\Composer;
 use Tkotosz\ComposerWrapper\Composer\Packages;
 
@@ -77,6 +79,34 @@ class ApplicationManager implements ApplicationManagerInterface
     public function removeExtension(string $extensionPackage): int
     {
         return $this->composer->removePackage($extensionPackage);
+    }
+
+    public function addExtensionSource(ExtensionSource $extensionSource): int
+    {
+        $config = $this->composer->getComposerConfig();
+
+        return $this->composer->changeComposerConfig(
+            $config->addRepository($extensionSource->name(), $extensionSource->type(), $extensionSource->url())
+        );
+    }
+
+    public function findExtensionSources(): ExtensionSources
+    {
+        $config = $this->composer->getComposerConfig();
+
+        $sources = [
+            ExtensionSource::fromValues('packagist.org', 'composer', 'https://repo.packagist.org')
+        ];
+
+        foreach ($config->repositories() as $name => $repository) {
+            $sources[] = ExtensionSource::fromValues(
+                $name,
+                $repository['type'],
+                $repository['url']
+            );
+        }
+
+        return ExtensionSources::fromItems($sources);
     }
 
     public function findInstalledExtensions(): Extensions
